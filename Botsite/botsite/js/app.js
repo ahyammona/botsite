a5b50c5b9cf7f0c3382042a3617ed697b2e788a7
ahyammona <37624177+ahyammona@users.noreply.github.com>
2020-06-29 17:44:27 -0700

    Add files via upload

---
+++ Botsite/botsite/js/app.js
@@ -0,0 +1,19 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+   navigator.serviceWorker.register('../sw.js').then( () => {
+    console.log('Service Worker Registered')
+   })
+ })
+}
+
+  function updateQuote(quoteNumber) {
+    var randomQuote = quotes[quoteNumber];
+      
+$("#quote").html(randomQuote.quote);
+    $("#author").html(randomQuote.author);
+    $("#quote-box").removeClass().addClass("animated bounceIn").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+      $(this).removeClass();
+    });
+  
+    $("#share").attr("href", "https://facebook.com/intent/tweet?text=\"" );
+  }  
